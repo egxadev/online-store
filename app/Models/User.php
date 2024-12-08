78cc89c85fe0bd95625868981f35a062eb43c567
Ega Alifya Firnando <eafirnando@genesysindonesia.com>
2024-12-08 14:11:19 +0700

    feat: crud modules

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +46,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function avatarUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => 'https://ui-avatars.com/api/?name=' . str_replace(' ', '+', $this->name) . '&background=4e73df&color=ffffff&size=100',
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,26 @@ Route::prefix('admin')->group(function () {
         
         //route dashboard
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+
+        //route category
+        Route::resource('/category', CategoryController::class, ['as' => 'admin']);
+
+        //route product
+        Route::resource('/product', ProductController::class, ['as' => 'admin']);
+
+        //route order
+        Route::resource('/order', OrderController::class, ['except' => ['create', 'store', 'edit', 'update', 'destroy'], 'as' => 'admin']);
+
+        //route customer
+        Route::get('/customer', [CustomerController::class, 'index'])->name('admin.customer.index');
+
+        //route slider
+        Route::resource('/slider', SliderController::class, ['except' => ['show', 'create', 'edit', 'update'], 'as' => 'admin']);
+
+        //profile
+        Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile.index');
+
+        //route user
+        Route::resource('/user', UserController::class, ['except' => ['show'], 'as' => 'admin']);
     });
 });
